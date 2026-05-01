7bf927681f2a4168831f8198ea48c0147b30d497
Todd Lam <todd.lam@prospa.com>
2026-05-01 17:29:03 +0700

    [implement-first-try] fix(httpapi): add missing Task25 test stubs per spec

--- api/internal/httpapi/artworks_test.go
+++ api/internal/httpapi/artworks_test.go
@@ -5,3 +5,15 @@ import "testing"
 func TestPlaceholder_FullMatrixInTask28(t *testing.T) {
 	t.Skip("privacy matrix tests are in Task 28")
 }
+
+func TestFeed_AnonHidesPrivate(t *testing.T) {
+	t.Skip("full matrix runner implemented in Task 28")
+}
+
+func TestArtworkDetail_NonOwnerGetsPrivate404(t *testing.T) {
+	t.Skip("full matrix runner implemented in Task 28")
+}
+
+func TestArtworkDetail_OwnerGetsSignedURLs(t *testing.T) {
+	t.Skip("full matrix runner implemented in Task 28")
+}
